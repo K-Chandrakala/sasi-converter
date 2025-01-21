be6bbeab7f262232a5e0fdcf832293b0aa5d1f19
K-Chandrakala <purna.kodeti@sasi.ac.in>
2025-01-21 14:55:05 +0530

    workflow

--- convert.test.js
+++ convert.test.js
@@ -1,5 +1,5 @@
 const convert = require('./convert') 
-test('converts 100 INR to USD correctly', () => {  expect(convert(100)).toBe('1.20'); 
+test('converts 100 INR to USD correctly', () => {  expect(convert(100)).toBe('1.200'); 
 }); 
 test('converts 0 INR to USD', () => { 
  expect(convert(0)).toBe('0.00'); 
