1dfe3f49ee926721ebd8f73850d6e2cdfe6e6ab0
K-Chandrakala <purna.kodeti@sasi.ac.in>
2025-01-21 16:10:18 +0530

    second commit

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const rl = readline.createInterface({
  output: process.stdout 
 }); 
 const promptUser = () => { 
- rl.question('Enter amount (INR): ', (input) => {  if (input.toLowerCase() === 'exit') {  console.log("Goodbye!"); 
+ rl.question('Enter amount (INR): ', (input) => {  
+    if (input.toLowerCase() === 'exit') {  console.log("Goodbye!"); 
  rl.close(); 
  return; 
  } 
  const inr = parseFloat(input); 
  try { 
  const usd = convert(inr); 
- console.log(`INR ${inr} = USD ${usd}\n`);  }catch(e){ 
+ console.log(`INR ${inr} = USD ${usd}\n`);  }
+ catch(e){ 
  console.log(e); 
  } 
  promptUser(); 
